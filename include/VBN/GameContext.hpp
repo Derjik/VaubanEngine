6c9eb9e18cc14700cba1cc29ad72b9337f8930dc
Julien Derjik Laurent <julien.laurent@engineer.com>
2018-10-15 18:20:06 +0200

    GameContext becomes an interface

--- include/VBN/GameContext.hpp
+++ include/VBN/GameContext.hpp
@@ -8,27 +8,11 @@ class HandlerResponse;
 
 class GameContext
 {
-	protected:
-		std::shared_ptr<WindowManager> _windowManager;
-
-		virtual void handleKeyboardEvent(
-				SDL_Event const & event,
-				std::shared_ptr<HandlerResponse> response);
-		virtual void handleMouseEvent(
-				SDL_Event const & event,
-				std::shared_ptr<HandlerResponse> response);
-		virtual void handleGameControllerEvent(
-				SDL_Event const & event,
-				std::shared_ptr<HandlerResponse> response);
-		virtual void handleWindowEvent(
-				SDL_Event const & event,
-				std::shared_ptr<HandlerResponse> response);
-
 	public:
-		GameContext(std::shared_ptr<WindowManager> wm);
+		virtual void handleEvent(
+			SDL_Event const & event,
+			std::shared_ptr<HandlerResponse> response) = 0;
 
-		void handleEvent(SDL_Event const & event,
-			std::shared_ptr<HandlerResponse> response);
 		virtual void draw(void) = 0;
 };
 
