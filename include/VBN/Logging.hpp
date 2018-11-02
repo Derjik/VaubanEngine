4cabdd2c0fca9a82cd10ef6ec7bddecde58fdf05
Julien Derjik Laurent <julien.laurent@engineer.com>
2018-11-02 18:59:06 +0100

    Fix wrong loglevel for DEBUG, use MSVC __FUNCTION__ when available

--- include/VBN/Logging.hpp
+++ include/VBN/Logging.hpp
@@ -3,23 +3,36 @@
 
 #include <SDL2/SDL_log.h>
 
+#ifdef _MSC_VER
+
+#define CRITICAL(category, format, ...) \
+		SDL_LogCritical(category, "%s:%d: " format, __FUNCTION__, __LINE__, __VA_ARGS__)
+#define ERROR(category, format, ...) \
+		SDL_LogError(category, "%s:%d: " format, __FUNCTION__, __LINE__, __VA_ARGS__)
+#define WARNING(category, format, ...) \
+		SDL_LogWarn(category, "%s:%d: " format, __FUNCTION__, __LINE__, __VA_ARGS__)
+#define INFO(category, format, ...) \
+		SDL_LogInfo(category, "%s:%d: " format, __FUNCTION__, __LINE__, __VA_ARGS__)
+#define DEBUG(category, format, ...) \
+		SDL_LogDebug(category, "%s:%d: " format, __FUNCTION__, __LINE__, __VA_ARGS__)
+#define VERBOSE(category, format, ...) \
+		SDL_LogVerbose(category, "%s:%d: " format, __FUNCTION__, __LINE__, __VA_ARGS__)
+
+#else
+
 #define CRITICAL(category, format, ...) \
 		SDL_LogCritical(category, "%s:%d: " format, __func__, __LINE__, __VA_ARGS__)
-
 #define ERROR(category, format, ...) \
 		SDL_LogError(category, "%s:%d: " format, __func__, __LINE__, __VA_ARGS__)
-
 #define WARNING(category, format, ...) \
 		SDL_LogWarn(category, "%s:%d: " format, __func__, __LINE__, __VA_ARGS__)
-
 #define INFO(category, format, ...) \
 		SDL_LogInfo(category, "%s:%d: " format, __func__, __LINE__, __VA_ARGS__)
-
 #define DEBUG(category, format, ...) \
-		SDL_LogError(category, "%s:%d: " format, __func__, __LINE__, __VA_ARGS__)
-
+		SDL_LogDebug(category, "%s:%d: " format, __func__, __LINE__, __VA_ARGS__)
 #define VERBOSE(category, format, ...) \
 		SDL_LogVerbose(category, "%s:%d: " format, __func__, __LINE__, __VA_ARGS__)
 
+#endif // _MSC_VER
 
 #endif // LOGGING_HPP_INCLUDED
