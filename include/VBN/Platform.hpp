d9662a686bac321071b4f54096dc45f4d5209734
Julien Derjik Laurent <julien.laurent@engineer.com>
2020-01-22 12:50:51 +0100

    Add Mixer to Platform (WIP)

--- include/VBN/Platform.hpp
+++ include/VBN/Platform.hpp
@@ -4,24 +4,28 @@
 #include <memory>
 #include <VBN/WindowManager.hpp>
 #include <VBN/GameControllerManager.hpp>
+#include <VBN/Mixer.hpp>
 
 class Platform
 {
 	private:
 		std::unique_ptr<WindowManager> _windowManager;
 		std::unique_ptr<GameControllerManager> _gameControllerManager;
+		std::unique_ptr<Mixer> _mixer;
 
 	public:
 		// May throw
 		Platform(
 			WindowManager * windowManager,
-			GameControllerManager * gameControllerManager);
+			GameControllerManager * gameControllerManager,
+			Mixer * mixer);
 		Platform(Platform const &) = delete;
 		Platform & operator = (Platform const &) = delete;
 		~Platform(void);
 
 		WindowManager * getWindowManager(void);
 		GameControllerManager * getGameControllerManager(void);
+		Mixer * getMixer(void);
 };
 
 #endif // PLATFORM_HPP_INCLUDED
