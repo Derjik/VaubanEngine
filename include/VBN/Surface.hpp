e01a2f5a21dd44cc7cfa9fad1704df221ae51774
Julien Derjik Laurent <julien.laurent@engineer.com>
2020-11-19 21:44:40 +0100

    Document Surface

--- include/VBN/Surface.hpp
+++ include/VBN/Surface.hpp
@@ -7,39 +7,54 @@
 
 class TrueTypeFontManager;
 
+/*!
+ * SDL_Surface wrapper class
+ *
+ * Each instance of this class encapsulates an SDL_Surface object in a dedicated
+ * std::unique_ptr, configured to free the underlying memory chunk using
+ * SDL_FreeSurface().
+ */
 class Surface
 {
 	private:
+		//! Underlying SDL_Surface enclosed in std::unique_ptr
 		std::unique_ptr<SDL_Surface, decltype(&SDL_FreeSurface)> _rawSurface;
-		// May throw
+
+		//! Private constructor (use factories for public instantiation)
 		Surface(SDL_Surface * rawSurface);
 
 	public:
-		Surface(Surface const & other) = delete;
+		//! Move a Surface instance
 		Surface(Surface && other);
-		Surface & operator = (Surface const &) = delete;
+		//! Delete a Surface instance
 		~Surface(void);
 
+		Surface(Surface const & other) = delete;
+		Surface & operator = (Surface const &) = delete;
+
+		//! Get raw pointer to underlying SDL_Surface
 		SDL_Surface * getSurface(void);
 
-		// May throw
+		//! Print a Latin1-encoded string onto a new Surface
 		static Surface fromLatin1Text(
 			std::shared_ptr<TrueTypeFontManager> ttfManager,
 			std::string const & text,
 			std::string const & fontName,
 			int const size,
 			SDL_Color const & color);
-		// May throw
+
+		//! Print an UTF-8-encoded string onto a new Surface
 		static Surface fromUTF8Text(
 			std::shared_ptr<TrueTypeFontManager> ttfManager,
 			std::string const & text,
 			std::string const & fontName,
 			int const size,
 			SDL_Color const & color);
-		// May throw
-		static Surface fromImage(
-			std::string const & path);
-		// May throw
+
+		//! Build a Surface from a disk-stored image
+		static Surface fromImage(std::string const & path);
+
+		//! Build a Surface from scratch using SDL parameters
 		static Surface fromScratch(
 			int const width,
 			int const height,
--- src/Surface.cpp
+++ src/Surface.cpp
@@ -4,11 +4,22 @@
 #include <VBN/Exceptions.hpp>
 #include <SDL2/SDL_image.h>
 
-Surface::Surface(SDL_Surface * rawSurface) : _rawSurface(rawSurface, &SDL_FreeSurface)
+/*!
+ * The main constructor for this class is private, external callers should use
+ * the "from...()" factories instead.
+ *
+ * @param	rawSurface	SDL_Surface raw pointer to encapsulate in the internal
+ *						std::unique_ptr (assuming ownership)
+ * @throws	Exception	nullptr passed for rawTexture parameter
+ */
+Surface::Surface(SDL_Surface * rawSurface) :
+	_rawSurface(rawSurface, &SDL_FreeSurface)
 {
+	// Check input parameters
 	if (rawSurface == nullptr)
 		THROW(Exception, "Received nullptr 'rawSurface'");
 
+	// Log
 	VERBOSE(SDL_LOG_CATEGORY_APPLICATION,
 		"Build Surface %p (SDL_Surface %p)",
 		this,
@@ -37,6 +48,15 @@ SDL_Surface * Surface::getSurface(void)
 	return _rawSurface.get();
 }
 
+/*!
+ * @param	ttfManager		Shared reference to the TrueTypeFontManager to use
+ * @param	text			Latin1-encoded string to print
+ * @param	fontName		Name of the font to use
+ * @param	size			Font size
+ * @param	color			Font color
+ * @returns					A newly created Surface with printed text
+ * @throws	Exception		Invalid input parameters or TTF call error
+ */
 Surface Surface::fromLatin1Text(
 			std::shared_ptr<TrueTypeFontManager> ttfManager,
 			std::string const & text,
@@ -44,6 +64,7 @@ Surface Surface::fromLatin1Text(
 			int const size,
 			SDL_Color const & color)
 {
+	// Check input parameters
 	if (!ttfManager)
 		THROW(Exception, "Received nullptr 'ttfManager'");
 	if (fontName.empty())
@@ -51,16 +72,28 @@ Surface Surface::fromLatin1Text(
 	if (size <= 0)
 		THROW(Exception, "Received 'size' <= 0");
 
+	// Try fetching font from TrueTypeFontManager
 	TrueTypeFont * font(ttfManager->getFont(fontName, size));
-	if (!font)
+	if (!font) /* No such font */
 		THROW(Exception,
 			"Cannot retrieve font '%s' size '%d'",
 			fontName,
 			size);
 
+	// Try rendering input text into a raw SDL_Surface, then encapsulate
+	// (may throw)
 	return Surface(font->renderSolidLatin1(text, color));
 }
 
+/*!
+ * @param	ttfManager		Shared reference to the TrueTypeFontManager to use
+ * @param	text			UTF-8-encoded string to print
+ * @param	fontName		Name of the font to use
+ * @param	size			Font size
+ * @param	color			Font color
+ * @returns					A newly created Surface with printed text
+ * @throws	Exception		Invalid input parameters or TTF call error
+ */
 Surface Surface::fromUTF8Text(
 			std::shared_ptr<TrueTypeFontManager> ttfManager,
 			std::string const & text,
@@ -68,6 +101,7 @@ Surface Surface::fromUTF8Text(
 			int const size,
 			SDL_Color const & color)
 {
+	// Check input parameters
 	if (!ttfManager)
 		THROW(Exception, "Received nullptr 'ttfManager'");
 	if (fontName.empty())
@@ -75,34 +109,54 @@ Surface Surface::fromUTF8Text(
 	if (size <= 0)
 		THROW(Exception, "Received 'size' <= 0");
 
+	// Try fetching font from TrueTypeFontManager
 	TrueTypeFont * font(ttfManager->getFont(fontName, size));
-	if (!font)
+	if (!font) /* No such font */
 		THROW(Exception,
 			"Cannot retrieve font '%s' size '%d'",
 			fontName,
 			size);
 
+	// Try rendering input text into a raw SDL_Surface, then encapsulate
+	// (may throw)
 	return Surface(font->renderSolidUTF8(text, color));
 }
 
+/*!
+ * @param	path	Full or relative path to image file
+ * @returns			A newly created Surface with the given image
+ * @throws			Invalid input parameters or SDL IMG call error
+ */
 Surface Surface::fromImage(std::string const & path)
 {
+	// Check input parameters
 	if (path.empty())
 		THROW(Exception, "Received empty 'path'");
 
+	// Try loading image in raw SDL_Surface
 	SDL_Surface * surface(IMG_Load(path.c_str()));
 
+	// Check for loading errors
 	if(surface == nullptr)
 		THROW(Exception,
 			"Cannot instantiate SDL_Surface : IMG error '%s'",
 			IMG_GetError());
 
+	// Return encapsulated Surface
 	return Surface(surface);
 }
 
-Surface Surface::fromScratch(int const width,
-			int const height,
-			int const depth)
+/*!
+ * @param	width	Surface width
+ * @param	height	Surface height
+ * @param	depth	Surface color depth
+ * @returns			Blank Surface
+ * @throws			SDL call error
+ */
+Surface Surface::fromScratch(
+	int const width,
+	int const height,
+	int const depth)
 {
 #if SDL_BYTEORDER == SDL_BIG_ENDIAN
 	Uint32 rMask = 0xFF000000;
@@ -116,20 +170,33 @@ Surface Surface::fromScratch(int const width,
 	Uint32 aMask = 0xFF000000;
 #endif
 
-	SDL_Surface * surface(SDL_CreateRGBSurface(0, width, height, depth,
-						rMask, gMask, bMask, aMask));
+	// Try creating blank surface from parameters
+	SDL_Surface * surface(
+		SDL_CreateRGBSurface(
+			0,
+			width,
+			height,
+			depth,
+			rMask,
+			gMask,
+			bMask,
+			aMask));
 
+	// Check for creation error
 	if(surface == nullptr)
 		THROW(Exception,
 			"Cannot instantiate SDL_Surface : SDL error '%s'",
 			SDL_GetError());
 
+	// Try setting alpha
 	Uint32 colorKey(SDL_MapRGBA(surface->format, 0, 0, 0, 0));
 
+	// Check for errors
 	if (SDL_SetColorKey(surface, SDL_TRUE, colorKey))
 		THROW(Exception,
 			"Failed to set colorkey : SDL error '%s'",
 			SDL_GetError());
 
+	// Return encapsulated surface
 	return Surface(surface);
 }
