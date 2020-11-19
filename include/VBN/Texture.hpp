23f9e768f07dcf917ef07fac4eba6c8462e2b304
Julien Derjik Laurent <julien.laurent@engineer.com>
2020-11-19 21:00:24 +0100

    Document Texture

--- include/VBN/Texture.hpp
+++ include/VBN/Texture.hpp
@@ -9,47 +9,70 @@
 class TrueTypeFontManager;
 class Surface;
 
+/*!
+ * SDL_Texture wrapper class
+ *
+ * Each instance of this class encapsulates an SDL_Texture object in a dedicated
+ * std::unique_ptr, configured to free the underlying memory chunk using
+ * SDL_DestroyTexture().
+ */
 class Texture
 {
 	private:
+		//! Underlying SDL_Texture enclosed in std::unique_ptr
 		std::unique_ptr<SDL_Texture, decltype(&SDL_DestroyTexture)> _rawTexture;
 
+		//! Named rectangles corresponding to tiles for sprite rendering
 		std::map<std::string, SDL_Rect> _clips;
 
+		//! SDL pixel format
 		Uint32 _pixelFormat;
+		//! SDL access mode
 		int _access;
+		//! Texture width
 		int _width;
+		//! Texture heigt
 		int _height;
 
+		//! Private constructor (use factories for public instantiation)
 		Texture(SDL_Texture * rawTexture);
 
 	public:
-		/* Constructors */
+		//! Move a Texture instance
 		Texture(Texture && other);
+		//! Assign-move a Texture instance
+		Texture & operator = (Texture && other);
+		//! Delete a Texture instance
+		~Texture(void);
 		Texture(Texture const & other) = delete;
 		Texture & operator = (Texture const &) = delete;
-		Texture & operator = (Texture && other);
-		~Texture(void);
 
-		/* Intrinsic and invariable attributes */
+		//! Get raw pointer to internal SDL_Texture
 		SDL_Texture * getSDLTexture(void);
+		//! Get Texture width
 		int getWidth(void) const;
+		//! Get Texture height
 		int getHeight(void) const;
+		//! Get Texture access mode
 		int getAccess(void) const;
+		//! Get Texture pixel format
 		Uint32 getPixelFormat(void) const;
 
-		/* Clips used for sprite rendering */
-		// May throw
-		void addClip(std::string const & clipName,
+		
+		//! Add a clipping rectangle to the clips dictionnary
+		void addClip(
+			std::string const & clipName,
 			SDL_Rect const & clip);
+
+		//! Get the clipping rectangle corresponding to a given name
 		SDL_Rect * getClip(std::string const & clipName);
 
-		/* Alpha modulation */
+		//! Set Color-Alpha modulation
 		void setColorAlphaMod(SDL_Color const & color);
+		//! Get Color-Alpha modulation
 		SDL_Color getColorAlphaMod(void) const;
 
-		/* Factories */
-		// May throw
+		//! Print a Latin1-encoded string onto a new Texture instance
 		static Texture fromLatin1Text(
 			std::shared_ptr<TrueTypeFontManager> ttfManager,
 			SDL_Renderer * renderer,
@@ -57,7 +80,8 @@ class Texture
 			std::string const & textFontName,
 			int const textSize,
 			SDL_Color const & textColor);
-		// May throw
+
+		//! Print an UTF-8-encoded string onto a new Texture instance
 		static Texture fromUTF8Text(
 			std::shared_ptr<TrueTypeFontManager> ttfManager,
 			SDL_Renderer * renderer,
@@ -65,11 +89,13 @@ class Texture
 			std::string const & textFontName,
 			int const textSize,
 			SDL_Color const & textColor);
-		// May throw
+
+		//! Build a Texture instance from an existing Surface instance
 		static Texture fromSurface(
 			SDL_Renderer * renderer,
 			Surface & surface);
-		// May throw
+
+		//! Build a Texture instance from scratch using SDL parameters
 		static Texture fromScratch(
 			SDL_Renderer * renderer,
 			Uint32 const format,
--- src/Texture.cpp
+++ src/Texture.cpp
@@ -4,6 +4,14 @@
 #include <VBN/Logging.hpp>
 #include <VBN/Exceptions.hpp>
 
+/*!
+ * The main constructor for this class is private, external callers should use
+ * the "from...()" factories instead.
+ *
+ * @param	rawTexture	SDL_Texture raw pointer to encapsulate in the internal
+ *						std::unique_ptr (assuming ownership)
+ * @throws	Exception	nullptr passed for rawTexture parameter
+ */
 Texture::Texture(SDL_Texture * rawTexture) :
 	_rawTexture(rawTexture, &SDL_DestroyTexture),
 	_pixelFormat(0),
@@ -11,17 +19,22 @@ Texture::Texture(SDL_Texture * rawTexture) :
 	_width(0),
 	_height(0)
 {
+	// Check input parameters
 	if(rawTexture == nullptr)
 		THROW(Exception, "Received nullptr 'rawTexture'");
 
-	SDL_QueryTexture(_rawTexture.get(),
-			&_pixelFormat,
-			&_access,
-			&_width,
-			&_height);
+	// Acquire SDL_Texture metrics
+	SDL_QueryTexture(
+		_rawTexture.get(),
+		&_pixelFormat,
+		&_access,
+		&_width,
+		&_height);
 
+	// Add a special, "global" clip matching the whole surface
 	_clips.emplace("", SDL_Rect{ 0, 0, _width, _height });
 
+	// Log
 	VERBOSE(SDL_LOG_CATEGORY_APPLICATION,
 		"Build Texture %p (SDL_Texture %p)",
 		this,
@@ -45,7 +58,7 @@ Texture::Texture(Texture && other) :
 
 Texture & Texture::operator=(Texture && other)
 {
-	// TODO : leak check below
+	//! @todo Leak check below
 	this->_rawTexture = std::move(other._rawTexture);
 	this->_clips = std::move(other._clips);
 	this->_pixelFormat = std::move(other._pixelFormat);
@@ -127,6 +140,19 @@ SDL_Color Texture::getColorAlphaMod(void) const
 	return rgba;
 }
 
+/*!
+ * @param	ttfManager		Shared reference on a valid TrueTypeFontManager from
+ *							which to extract the TrueTypeFont to use
+ * @param	renderer		Raw pointer to the SDL_Renderer to use
+ * @param	text			Latin1-encoded string to print
+ * @param	textFontName	Font name
+ * @param	textSize		Font size
+ * @param	textColor		Font color
+ * @returns					A newly created Texture instance containing the
+ *							text render
+ * @throws	Exception		Invalid input parameters, Surface instantiation
+ *							or conversion error
+ */
 Texture Texture::fromLatin1Text(
 	std::shared_ptr<TrueTypeFontManager> ttfManager,
 	SDL_Renderer * renderer,
@@ -135,6 +161,7 @@ Texture Texture::fromLatin1Text(
 	int const textSize,
 	SDL_Color const & textColor)
 {
+	// Check input parameters
 	if (!ttfManager)
 		THROW(Exception, "Received nullptr 'ttfManager'");
 	if (renderer == nullptr)
@@ -144,6 +171,7 @@ Texture Texture::fromLatin1Text(
 	if (textSize <= 0)
 		THROW(Exception, "Received textSize <= 0");
 
+	// Instantiate Text Surface
 	Surface textSurface(
 		Surface::fromLatin1Text(
 			ttfManager,
@@ -151,9 +179,24 @@ Texture Texture::fromLatin1Text(
 			textFontName,
 			textSize,
 			textColor));
+
+	// Attempt conversion to Texture
 	return Texture::fromSurface(renderer, textSurface);
 }
 
+/*!
+ * @param	ttfManager		Shared reference on a valid TrueTypeFontManager from
+ *							which to extract the TrueTypeFont to use
+ * @param	renderer		Raw pointer to the SDL_Renderer to use
+ * @param	text			UTF-8-encoded string to print
+ * @param	textFontName	Font name
+ * @param	textSize		Font size
+ * @param	textColor		Font color
+ * @returns					A newly created Texture instance containing the
+ *							text render
+ * @throws	Exception		Invalid input parameters, Surface instantiation
+ *							or conversion error
+ */
 Texture Texture::fromUTF8Text(
 	std::shared_ptr<TrueTypeFontManager> ttfManager,
 	SDL_Renderer * renderer,
@@ -162,6 +205,7 @@ Texture Texture::fromUTF8Text(
 	int const textSize,
 	SDL_Color const & textColor)
 {
+	// Check input parameters
 	if (!ttfManager)
 		THROW(Exception, "Received nullptr 'ttfManager'");
 	if (renderer == nullptr)
@@ -171,6 +215,7 @@ Texture Texture::fromUTF8Text(
 	if (textSize <= 0)
 		THROW(Exception, "Received textSize <= 0");
 
+	// Instantiate Text Surface
 	Surface textSurface(
 		Surface::fromUTF8Text(
 			ttfManager,
@@ -178,27 +223,50 @@ Texture Texture::fromUTF8Text(
 			textFontName,
 			textSize,
 			textColor));
+
+	// Attempt conversion to Texture
 	return Texture::fromSurface(renderer, textSurface);
 }
 
+/*!
+ * @param	renderer	Raw pointer to the SDL_Renderer to use
+ * @param	surface		Reference to the Surface instance to convert
+ * @returns				A newly created Texture built from the input Surface
+ *						contents
+ * @throws	Exception	Invalid input parameters or SDL_Surface to SDL_Texture
+ *						conversion error
+ */
 Texture Texture::fromSurface(
 	SDL_Renderer * renderer,
 	Surface & surface)
 {
+	// Check input parameters
 	if (renderer == nullptr)
 		THROW(Exception, "Received nullptr 'renderer'");
 
+	// Attempt SDL_Surface to SDL_Texture conversion
 	SDL_Texture * rawTexture =
 		SDL_CreateTextureFromSurface(renderer, surface.getSurface());
 
+	// Check for failure
 	if (rawTexture == nullptr)
 		THROW(Exception,
 			"Cannot instantiate SDL_Texture : SDL error '%s'",
 			SDL_GetError());
 
+	// Return encapsulated Texture
 	return Texture(rawTexture);
 }
 
+/*!
+ * @param	renderer	Raw pointer to the SDL_Renderer to use
+ * @param	format		Pixel format
+ * @param	access		Access mode
+ * @param	width		Width
+ * @param	height		Height
+ * @returns				A blank Texture created from input parameters
+ * @throws				Invalid input parameters or SDL error
+ */
 Texture Texture::fromScratch(
 	SDL_Renderer * renderer,
 	Uint32 const format,
@@ -206,6 +274,7 @@ Texture Texture::fromScratch(
 	int const width,
 	int const height)
 {
+	// Check input parameters
 	if (renderer == nullptr)
 		THROW(Exception, "Received nullptr 'renderer'");
 	if (width <= 0)
@@ -213,6 +282,7 @@ Texture Texture::fromScratch(
 	if (height <= 0)
 		THROW(Exception, "Received 'height' <= 0");
 
+	// Attempt SDL_Texture creation from scratch
 	SDL_Texture * rawTexture = SDL_CreateTexture(
 		renderer,
 		format,
@@ -220,17 +290,26 @@ Texture Texture::fromScratch(
 		width,
 		height);
 
+	// Check for failure
 	if(rawTexture == nullptr)
 		THROW(Exception,
 			"Cannot instantiate SDL_Texture : SDL error '%s'",
 			SDL_GetError());
 
+	// Return encapsulated Texture
 	return Texture(rawTexture);
 }
 
-void Texture::addClip(std::string const & name,
+/*!
+ * @param	name		Human-readable name to associate with the clipping rectangle
+ * @param	clip		SDL_Rect representing the clipping area
+ * @throws	Exception	Invalid input parameters
+ */
+void Texture::addClip(
+	std::string const & name,
 	SDL_Rect const & clip)
 {
+	// Check input parameters
 	if (name.empty())
 		THROW(Exception, "Received empty 'name'");
 	if(_clips.find(name) != _clips.end())
@@ -238,14 +317,21 @@ void Texture::addClip(std::string const & name,
 			"Cannot override existing clip '%s'",
 			name);
 
+	// Store
 	_clips.emplace(name, clip);
 }
 
+/*!
+ * @param	name	Human-readable name of the wanted clip
+ * @returns			Raw pointer to the SDL_Rect clip if it exists, nullptr
+ *					otherwise
+ */
 SDL_Rect * Texture::getClip(std::string const & name)
 {
+	// Clip lookup
 	auto clipIterator = _clips.find(name);
-	if (clipIterator == _clips.end())
+	if (clipIterator == _clips.end()) /* Miss */
 		return nullptr;
 	else
-		return (&clipIterator->second);
+		return (&clipIterator->second); /* Hit */
 }
