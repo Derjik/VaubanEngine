19792aeb7fe838c6b85d00270d15a2805f6fb9d5
Julien "Derjik" Laurent <julien.laurent@engineer.com>
2020-11-22 22:28:27 +0100

    Begin Platform documentation

--- include/VBN/Platform.hpp
+++ include/VBN/Platform.hpp
@@ -6,25 +6,40 @@
 #include <VBN/GameControllerManager.hpp>
 #include <VBN/Mixer.hpp>
 
+/*!
+ * Aggregates global platform elements : WindowManager, GameControllerManager,
+ * Mixer
+ *
+ * This class holds global handles to the main components of the current 
+ * running platform
+ */
 class Platform
 {
 	private:
+		//! Stores & manages Window instances
 		std::unique_ptr<WindowManager> _windowManager;
+		//! Stores & manages GameController instances
 		std::unique_ptr<GameControllerManager> _gameControllerManager;
+		//! Handles sound effects & music playback
 		std::unique_ptr<Mixer> _mixer;
 
 	public:
-		// May throw
+		//! Build a Platform
 		Platform(
 			WindowManager * windowManager,
 			GameControllerManager * gameControllerManager,
 			Mixer * mixer);
-		Platform(Platform const &) = delete;
-		Platform & operator = (Platform const &) = delete;
+		//! Delete a Platform
 		~Platform(void);
 
+		Platform(Platform const &) = delete;
+		Platform & operator = (Platform const &) = delete;
+
+		//! Get a raw pointer to the WindowManager
 		WindowManager * getWindowManager(void);
+		//! Get a raw pointer to the GameControllerManager
 		GameControllerManager * getGameControllerManager(void);
+		//! Get a raw pointer to the Mixer
 		Mixer * getMixer(void);
 };
 
